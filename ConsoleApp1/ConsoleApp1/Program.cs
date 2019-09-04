cdfce9750ce95e000aba0001ec64fb63500ff227
Dillon Zetzer <dillon.zetzer@sbcglobal.net>
2019-09-04 03:19:09 -1100

    Committed original solution

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Dillon";
+            int age = 21;
+            Console.WriteLine(firstName);
+            Console.WriteLine(age);
+            Console.ReadLine();
+        }
+    }
+}
