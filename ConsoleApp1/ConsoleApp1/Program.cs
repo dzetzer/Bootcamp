cc7f81adf78b3317a240261c5579a23ec9fecf36
Dillon Zetzer <dillon.zetzer@sbcglobal.net>
2019-09-04 07:15:15 -1100

    did name comparison exercise, created .net core webapp project

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,11 +6,41 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            string firstName = "Dillon";
-            int age = 21;
-            Console.WriteLine(firstName);
-            Console.WriteLine(age);
-            Console.ReadLine();
+            int appState = 0;
+            string firstName = "";
+            string lastName = "";
+
+            //Display
+            while (appState != -1)
+            {
+                //Headsup Display
+                Console.WriteLine("First Name: " + firstName + ", Last Name: " + lastName);
+                //Get First Name
+                switch(appState)
+                {
+                    case 0:
+                        Console.WriteLine("Please enter your first name");
+                        firstName = Console.ReadLine();
+                        appState = 1;
+                        break;
+                    case 1:
+                        Console.WriteLine("Please enter your last name");
+                        lastName = Console.ReadLine();
+                        appState = 2;
+                        break;
+                    case 2:
+                        if (firstName.Length > lastName.Length) Console.WriteLine("Your First Name is longer than your Last Name");
+                        if (firstName.Length < lastName.Length) Console.WriteLine("Your Last Name is longer than your First Name");
+                        if (firstName.Length == lastName.Length) Console.WriteLine("Your First and Last Names are equal in Length");
+                        Console.WriteLine("Press Enter to Reset");
+                        Console.ReadLine();
+                        firstName = "";
+                        lastName = "";
+                        appState = 0;
+                        break;
+                }
+                Console.Clear();
+            }
         }
     }
 }
