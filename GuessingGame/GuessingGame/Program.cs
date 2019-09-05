1b275183d3dc83f38eba5f19630ec38609418a53
Dillon Zetzer <dillon.zetzer@sbcglobal.net>
2019-09-05 02:58:44 -1100

    Guessing Game step 2

--- GuessingGame/GuessingGame/Program.cs
+++ GuessingGame/GuessingGame/Program.cs
@@ -6,14 +6,22 @@ namespace GuessingGame
     {
         static void Main(string[] args)
         {
-            int numGuessed;
-            bool gameWon = false;
+            while(1 == 1)
+            {
+                int numGuessed;
+                bool gameWon = false;
 
-            Console.WriteLine("Enter a number between 1 and 10");
-            numGuessed = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter a number between 1 and 10");
+                numGuessed = Convert.ToInt32(Console.ReadLine());
 
-            if (numGuessed == 7) Console.WriteLine("You Won!");
-            else Console.WriteLine("You Lost!");
+                if (numGuessed == 7) Console.WriteLine("You Won!");
+                else if (numGuessed == 0) Console.WriteLine("Remember to enter a number between 1 and 10");
+                else Console.WriteLine("You Lost!");
+                
+                
+                Console.ReadLine();
+                Console.Clear();
+            }
         }
     }
 }
