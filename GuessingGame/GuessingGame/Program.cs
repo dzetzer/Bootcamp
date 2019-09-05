73575f2cbbd3e22a91e88e371364f74fa40003e8
Dillon Zetzer <dillon.zetzer@sbcglobal.net>
2019-09-05 04:07:52 -1100

    Guessing Game step 5

--- GuessingGame/GuessingGame/Program.cs
+++ GuessingGame/GuessingGame/Program.cs
@@ -15,14 +15,24 @@ namespace GuessingGame
                 Console.WriteLine("Enter a number between 1 and 10");
                 numGuessed = Convert.ToInt32(Console.ReadLine());
 
+                //Guess Responses
                 if (numGuessed == 7) gameWon = true;
                 else if (numGuessed == 0) Console.WriteLine("Remember to enter a number between 1 and 10");
                 else if (numGuessed == -1) Environment.Exit(1);
                 else Console.WriteLine("You Lost!");
 
-                if(gameWon == true) Console.WriteLine("You Won!");
+                //Win Game
+                if (gameWon == true)
+                {
+                    Console.WriteLine("You Won!");
+                    guessAttempts = 2;
+                }
                 else
                 {
+                    //Show if Greater or Less
+                    if (numGuessed > 7) Console.WriteLine("Your number is greater than the correct number");
+                    if (numGuessed < 7) Console.WriteLine("Your number is less than the correct number");
+                    //Restart
                     Console.ReadLine();
                     Console.Clear();
                     guessAttempts++;
