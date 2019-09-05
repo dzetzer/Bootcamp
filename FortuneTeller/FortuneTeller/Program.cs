af65787401a3ebda5555606e08b62b9ccf39a8a4
Dillon Zetzer <dillon.zetzer@sbcglobal.net>
2019-09-05 04:20:45 -1100

    Fortune Teller step 1

---
+++ FortuneTeller/FortuneTeller/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace FortuneTeller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Get Personal Info
+            string firstName;
+            string lastName;
+            string age;
+            string birthMonth;
+            string favColor;
+            string sibNumber;
+
+            Console.WriteLine("What is your first name?");
+            firstName = Console.ReadLine();
+
+            Console.WriteLine("What is your last name?");
+            lastName = Console.ReadLine();
+
+            Console.WriteLine("How old are you?");
+            age = Console.ReadLine();
+
+            Console.WriteLine("What number month were you born?");
+            birthMonth = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite color");
+            favColor = Console.ReadLine();
+
+            Console.WriteLine("How many siblings do you have?");
+            sibNumber = Console.ReadLine();
+        }
+    }
+}
